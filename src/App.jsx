b02b118582a72317dff86d7e8d677fa08e32d19f
alexquar <alexanderquarrie@gmail.com>
2024-04-02 12:16:08 -0400

    setup for flow

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 
 import './App.css';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar';
+import Login from './pages/login/Login';
+import Dashboard from './pages/dashboard/Dashboard';
+import Create from './pages/create/Create';
+import Signup from './pages/signup/Signup';
+import Project from './pages/project/Project';
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
+      <div className="container">
+   <Navbar />
+      <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/create" element={<Create />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/project:id" element={<Project />} />
+  </Routes>
+  </div>
+  </div>
+    </BrowserRouter>
+  
 
-    </div>
   );
 }
 
