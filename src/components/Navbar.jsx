a42d55d4cae0c0d1bc1286d59de17be9e47da424
alexquar <alexanderquarrie@gmail.com>
2024-04-02 12:34:26 -0400

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,21 @@
 import './Navbar.css'
-
+import icon from '../assets/icon.png'
+import { NavLink } from 'react-router-dom'
 export default function Navbar() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    return (
+      <nav className="navbar">
+        <ul>
+          <li className="logo">
+            <img src={icon} alt="dojo logo" />
+            <span><h1>U Projects</h1></span>
+          </li>
+  
+          <li><NavLink to="/login">Login</NavLink></li>
+          <li><NavLink to="/signup">Signup</NavLink></li>
+          <li>
+            <button className="btn">Logout</button>
+          </li>
+        </ul>
+      </nav>
+    )
+  }
