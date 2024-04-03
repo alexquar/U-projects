4e9d9cd8c1d8e7482bc9133c6b664fbdcad8d4bb
alexquar <alexanderquarrie@gmail.com>
2024-04-03 09:55:46 -0400

    dots

--- src/components/OnlineUsers.jsx
+++ src/components/OnlineUsers.jsx
@@ -5,13 +5,16 @@ export default function OnlineUsers() {
     const {documents, error} = useCollection('user')
   return (
     <div className='user-list'>
-      <h2>Oneline Users</h2>
+      <h2>Oneline/Offline Users</h2>
 
     {
       documents &&  documents.map(user => (
             <div className='user-list-item' key={user.id}>
+         {user.online && <span className="online-user"></span>}
+             {!user.online &&   <span className="offline-user"></span>}
                 <span>{user.displayName}</span>
                 <Avatar src={user.photoURL}/>
+                
             </div>
         ))
     }
