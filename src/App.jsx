b0340cc7ec2ad5b6361a875b58411bb8ef1ef6e3
alexquar <alexanderquarrie@gmail.com>
2024-04-02 21:53:30 -0400

    navbar conditions

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,33 @@ import Create from './pages/create/Create';
 import Signup from './pages/signup/Signup';
 import Project from './pages/project/Project';
 import Sidebar from './components/Sidebar';
+import { Navigate } from 'react-router-dom';
+import { useAuthContext } from './hooks/useAuthContext';
 function App() {
+  const {user, authIsReady} = useAuthContext()
   return (
     
     <div className="App">
+      {authIsReady &&
       <BrowserRouter>
       <Sidebar />
       <div className="container">
    <Navbar />
       <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/create" element={<Create />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/project:id" element={<Project />} />
+    {user &&  <Route path="/" element={<Dashboard />} />}
+    {!user &&  <Route path="/" element={<Navigate to='/login' />} />}
+    {!user && <Route path="/login" element={<Login />} /> }
+    {user && <Route path="/login" element={<Navigate to='/' />} /> }
+    {user &&  <Route path="/create" element={<Create />} /> }
+    {!user &&  <Route path="/create" element={<Navigate to='/login' />} /> }
+    {!user &&  <Route path="/signup" element={<Signup />} /> }
+    {user && <Route path="/signup" element={<Navigate to='/' />} /> }
+    {user && <Route path="/project:id" element={<Project />} /> }
+    {!user && <Route path="/project:id" element={<Navigate to='/login' />} /> }
   </Routes>
   </div>
   </BrowserRouter>
+}
   </div>
   
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,22 +2,28 @@ import './Navbar.css'
 import icon from '../assets/icon.png'
 import { NavLink } from 'react-router-dom'
 import {useLogout} from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext';
 export default function Navbar() {
   const {logout, isPending} = useLogout()
+  const {user} = useAuthContext()
     return (
       <nav className="navbar">
         <ul>
           <li className="logo">
             <img src={icon} alt="dojo logo" />
-            <span><h1>U Projects</h1></span>
+           <NavLink id="Title"><span><h1>U Projects</h1></span></NavLink> 
           </li>
-  
+        {!user && 
           <li><NavLink to="/login">Login</NavLink></li>
+        }
+        {!user &&
           <li><NavLink to="/signup">Sign up</NavLink></li>
-          <li>
+        }
+        {user &&<li>
           {!isPending && <button onClick={logout} className="btn">Logout</button>}
           {isPending && <button disabled className="btn">Pending...</button>}
           </li>
+       }
         </ul>
       </nav>
     )
