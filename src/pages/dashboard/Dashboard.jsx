9c18e687f0fa0abf9d04b10451206d1529b206b7
alexquar <alexanderquarrie@gmail.com>
2024-04-04 10:14:14 -0400

    filter component

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -5,6 +5,7 @@ import ProjectList from '../../components/ProjectList'
 
 // styles
 import './Dashboard.css'
+import ProjectFilter from './ProjectFilter'
 
 export default function Dashboard() {
   const { documents, error } = useCollection('projects')
@@ -13,6 +14,7 @@ export default function Dashboard() {
     <div>
       <h1 className="page-title" id='dashtitle'>Dashboard</h1>
       {error && <p className="error">{error}</p>}
+      {documents && <ProjectFilter />}
       {documents && <ProjectList projects={documents} />}
     </div>
   )
---
+++ src/pages/dashboard/ProjectFilter.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+
+
+const filterList = ['all', 'mine', 'development', 'design', 'marketing', 'sales']
+export default function ProjectFilter() {
+const [currentFilter, setCurrentFilter] = useState()
+const handleClick = (newFilter) => {
+    setCurrentFilter(newFilter)
+}
+
+  return (
+    <div className="project-filter">
+       <nav>
+        <p>Filter by: </p>
+        {filterList.map((f) => (
+          <button key={f}
+            onClick={() => handleClick(f)}
+            className={currentFilter === f ? 'active' : ''}
+          >{f}</button>
+        ))}
+      </nav>
+    </div>
+  )
+}
