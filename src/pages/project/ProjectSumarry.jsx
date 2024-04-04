cc2d5e63b8741a759229c7c5763a2220e0048c43
alexquar <alexanderquarrie@gmail.com>
2024-04-04 09:35:19 -0400

    complete proj

--- src/pages/project/ProjectSumarry.jsx
+++ src/pages/project/ProjectSumarry.jsx
@@ -1,11 +1,22 @@
 import Avatar from "../../components/Avatar"
+import { useAuthContext } from "../../hooks/useAuthContext"
+import { useFirestore } from "../../hooks/useFirestore"
+import { useNavigate } from "react-router-dom"
 export default function ProjectSumarry({project}) {
+  const {deleteDocument} = useFirestore('projects')
+  const {user} = useAuthContext()
+  const navigate = useNavigate()
+  const handleClick = () => {
+    deleteDocument(project.id)
+    navigate('/')
+  }
   return (
     <div>
       <div className="project-summary">
         <h2 className="page-title">{project.name}</h2>
+        <p>By {project.createdBy.displayName}</p>
         <p className="due-date">
-          Project due by {project.dueDate.toDate().toDateString()}
+          Project due by {project.dueDate.toDate().toDateString()}.
         </p>
         <p className="details">
           {project.details}
@@ -19,6 +30,7 @@ export default function ProjectSumarry({project}) {
           ))}
         </div>
       </div>
+      { project.createdBy.id === user.uid && <button className="btn" onClick={handleClick}>Complete/Remove Project</button> }
     </div>
   )
 }
